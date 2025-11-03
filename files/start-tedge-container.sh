f23db6d5bd337d028e0d0afdc418481fe6b9d604
ck-c8y <christof.strack@cumulocity.com>
2025-11-03 18:26:03 +0100

    update script

--- files/start-tedge-container.sh
+++ files/start-tedge-container.sh
@@ -105,5 +105,6 @@ podman run -d \
 -e TEDGE_MQTT_BRIDGE_BUILT_IN=true \
 -e TEDGE_DEVICE_CERT_PATH=/local-conf/tedge-certificate.pem \
 -e TEDGE_DEVICE_KEY_PATH=/local-conf/tedge-private-key.pem \
+-v "/run/user/$UID/podman/podman.sock:/var/run/docker.sock:rw" \
 --env-file /home/tedge/edge/config/iot.cfg \
 "$FULL_IMAGE":"$HIGHEST_TAG"
